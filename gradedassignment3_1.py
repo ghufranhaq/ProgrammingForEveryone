41a401cf5cf826c1432b9e054ee75ddf7c93c121
Ghufran <ghufran.haq@gmail.com>
2018-11-16 00:08:40 +0500

    test commit

---
+++ gradedassignment3_1.py
@@ -0,0 +1,10 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+
+rate = input("Enter rate per Hours:")
+r = float(rate)
+
+if h <= 40:
+ print (h*r)
+    elif h => 40;
+     print (h*r + (5*10.5))
---
+++ practice3.py
@@ -0,0 +1,18 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+
+rate = input("Enter rate per Hours:")
+r = float(rate)
+
+diff = (h-40)
+
+if h <= 40:
+    pay = (h * r)
+    print(pay)
+
+elif h >= 40:
+ rateabove40 = (1.5 * r)
+ Pay40 = (h * r)
+
+ #pay = (h * r) +
+ print(Pay40)
